a937f208e88bf9270167be035336700826c7dd7e
Marian4gc <mariangarcia1724@gmail.com>
2023-10-18 11:20:11 +0200

    changes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
                 </div>
                 <div className="col-lg-6">
                     <h1 className="display-5 fw-bold lh-1 mb-3 who">Who is Marian?</h1>
-                        <p className="lead">Hi! I am a woman who has just entered the world of coding and discovered my passion for it. I enjoy identifying and solving problems while continuously learning. Overcoming challenges and bringing my design ideas to life in the form of web applications is what excites me. I have completed an 850-hour Full-Stack Developer Bootcamp, but my curiosity to keep learning and advancing remains insatiable. Feel free to get in touch if you'd like to connect.</p>
+                        <p className="lead_about">Hi! I am a woman who has just entered the world of coding and discovered my passion for it. I enjoy identifying and solving problems while continuously learning. Overcoming challenges and bringing my design ideas to life in the form of web applications is what excites me. I have completed an 850-hour Full-Stack Developer Bootcamp, but my curiosity to keep learning and advancing remains insatiable. Feel free to get in touch if you'd like to connect.</p>
                 </div>
             </div>
     </div>
